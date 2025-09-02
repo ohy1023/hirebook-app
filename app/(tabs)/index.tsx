d70031beb031ef11da40a22da326110cc791d4d6
오형상 <vyg1023@gmail.com>
2025-09-02 11:25:20 +0900

    [Feat] 하단 탭 추가

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function RecordScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>오늘 기록 화면</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    text: { fontSize: 20 },
+});
